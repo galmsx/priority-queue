73a1c2b787d9121ee508d34702e3ed8f1a23ee14
galmsx <mgalinovski@gmail.com>
2019-03-08 00:39:24 +0300

    aaaaa

--- src/node.js
+++ src/node.js
@@ -1,23 +1,42 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	}
+  appendChild(node) {
+    if (!this.left) this.left = node;
+    else if (!this.right) this.right = node;
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    if (node == this.left) this.left = null;
+    else if (node == this.right) this.right = null;
+    else throw Error("this node is not a child!");
+    node.parent = null;
+  }
 
-	}
+  remove() {
+    if (this.parent == null) return;
+	this.parent.removeChild(this);
+	this.left.removeChild(this.left);
+	this.right.removeChild(this.right);
+  }
 
-	removeChild(node) {
+  swapWithParent() {
+	if (this.parent == null) return;
+	var parent = this.parent;
+    if (this.parent.left == this) {
+		this.parent = parent.parent;
+		parent.parent = this;
+		this.left = parent;
 
-	}
-
-	remove() {
-
-	}
-
-	swapWithParent() {
-		
-	}
+    } else {
+    }
+  }
 }
 
 module.exports = Node;
